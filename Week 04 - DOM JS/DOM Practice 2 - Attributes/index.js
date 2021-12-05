14c7e7f0a18e2ce374b6f4da94b8dc6a6042a2e7
FOD17 <w_mark81@yahoo.com>
2021-12-05 11:56:25 -0600

    more js work

--- Week 04 - DOM JS/DOM Practice 2 - Attributes/index.js
+++ Week 04 - DOM JS/DOM Practice 2 - Attributes/index.js
@@ -4,6 +4,7 @@
     childElementCount
     childNodes
     children
+    className
     closest
     firstChild
     firstElementChild
@@ -28,3 +29,38 @@ let appendedElement = document.createElement('h2');
 let text = document.createTextNode('MAGIC!');
 appendedElement.appendChild(text);
 document.getElementById('node').appendChild(appendedElement);
+
+/**
+ * Example 2
+ * childElementCount
+ * */
+let elementKidCount = document.getElementById('list').childElementCount;
+console.log('The List has this man children: ', elementKidCount);
+
+/**
+ * Example 3
+ * childNodes (notice it returns more elements than you expect)
+ * */
+let elementKidNodes = document.getElementById('list').childNodes;
+console.log(elementKidNodes);
+
+/**
+ * Example 4
+ * childNodes (notice it returns what you expect)
+ * */
+let elementChildren = document.getElementById('list').children;
+console.log(elementChildren);
+
+/**
+ * Example 5
+ * className
+ * */
+document.getElementById('node').className = 'updated-class';
+
+/**
+ * Example 6
+ * closest
+ * use #h1 and #p to show it overlooks siblings
+ * */
+let closestThing = document.getElementById('list').closest('#body');
+console.log(closestThing);
