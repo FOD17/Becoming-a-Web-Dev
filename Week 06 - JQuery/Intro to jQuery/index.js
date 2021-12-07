1fb89e4ec5e5911c560ef217bd37401b602e8cfe
Mark Williams <markwilliams@marks-mbp-2.lan>
2021-12-06 19:59:18 -0600

    jquery stuff

---
+++ Week 06 - JQuery/Intro to jQuery/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  $("#hide").click(function () {
+    $("#example").hide()
+  })
+
+  $("#show").click(function () {
+    $("#example").show()
+  })
+
+  $("#fade").click(function () {
+    $("#example").fadeOut("slow").fadeIn("slow")
+  })
+
+  $("#slide").click(function () {
+    $("#example").slideUp().slideDown(3000)
+  })
+
+  $("#animate-me").click(function () {
+    $("#example").animate({
+      left: "250px",
+      color: "green",
+    })
+  })
+
+  $("#animate-revert").click(function () {
+    $("#example").animate({
+      left: "0px",
+      color: "white",
+    })
+  })
+
+  $("#get-val").click(function () {
+    alert($("#example").attr("id"))
+  })
+
+  $("#get-dim").click(function () {
+    alert($("#example").width())
+  })
+})
