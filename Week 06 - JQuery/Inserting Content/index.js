b8f3aaa56db5e143b365a0bc168fdeedb42193d6
FOD17 <w_mark81@yahoo.com>
2021-12-24 14:43:17 -0600

    jq update

--- Week 06 - JQuery/Inserting Content/index.js
+++ Week 06 - JQuery/Inserting Content/index.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
   // Add Text
-  $('#h1').click(() => {
+  $('#h1-text').click(() => {
     $('#main').append('This is new...');
   });
 
-  // Add Content: adds to inside of the element (nested)
+  // Add Content: adds to inside of element (nested)
   $('#h1-content').click(() => {
     let heading = '<h1>Added Text:</h1>';
-    var paragraph = document.createElement('p');
+    let paragraph = document.createElement('p');
     paragraph.innerHTML = '<em>I am ready for six figures!</em>';
     $('body').append(heading, paragraph);
   });
 
-  // Add Content Before
+  // Add Cotent Before
   $('#h1-content-before').click(() => {
     let heading = '<h1>Added Text:</h1>';
-    var paragraph = document.createElement('p');
+    let paragraph = document.createElement('p');
     paragraph.innerHTML = '<em>I am ready for six figures!</em>';
     $('#main').before(heading, paragraph);
   });
 
-  // Add Content After
+  // Add Cotent After
   $('#h1-content-after').click(() => {
     let heading = '<h1>Added Text:</h1>';
-    var paragraph = document.createElement('p');
+    let paragraph = document.createElement('p');
     paragraph.innerHTML = '<em>I am ready for six figures!</em>';
     $('#main').after(heading, paragraph);
   });
