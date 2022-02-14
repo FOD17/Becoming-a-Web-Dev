318d9776a28631abedd09318bb34662da4fae4fb
Mark Williams <markwilliams@Marks-MacBook-Pro-2.local>
2022-02-14 16:13:40 -0600

    bootstrap progress

--- Week 04 - DOM JS/Basic JavaScript/index.js
+++ Week 04 - DOM JS/Basic JavaScript/index.js
@@ -5,42 +5,42 @@
 // We can also do this in the web console, replit, or in node
 
 // Variables - var, let, const
-var myName = 'Bernie';
-let dogAge = 1;
-let myBreed = 'Havanese';
-const cute = True;
+var myName = "Bernie"
+let dogAge = 1
+let myBreed = "Havanese"
+const cute = True
 
 // Show something is "private"
-var _favoriteFood = 'pizza';
+var _favoriteFood = "pizza"
 
 // Array
-let todoList = ['Sleep', 'Walk Bernie', 'Sleep'];
+let todoList = ["Sleep", "Walk Bernie", "Sleep"]
 
 // Object
-const dog = { name: 'Bernie', breed: 'havanese' };
-dog.colors = 'black and white';
+const dog = { name: "Bernie", breed: "havanese" }
+dog.colors = "black and white"
 
 // Conditionals
-const x = 11;
+const x = 11
 if (x > 10) {
   // do something
-} else if (X > 5) {
+} else if (x > 5) {
   // do something
 } else {
   // do something
 }
 
-const value = 5;
+const value = 5
 switch (value) {
   case 5: // do something
-    break;
+    break
 
   case 4:
-    break;
+    break
 
   default:
     // do default thing or nothing at all
-    break;
+    break
 }
 
 // Loops - for, for/in, for/of, while, do/while
@@ -48,37 +48,37 @@ for (let i = 0; i < 5; i++) {
   // do something
 }
 
-let todoList = ['Sleep', 'Walk Bernie', 'Sleep'];
+let todoList = ["Sleep", "Walk Bernie", "Sleep"]
 for (const item of todoList) {
-  console.log(item);
+  console.log(item)
 }
 
-const dog = { dogOne: 'Bernie', dogTwo: 'Fido', dogThree: 'Woof Woof' };
+const dog = { dogOne: "Bernie", dogTwo: "Fido", dogThree: "Woof Woof" }
 for (const key in dog) {
-  console.log(key, dog[key]);
+  console.log(key, dog[key])
 }
 
-let x = 20;
+let x = 20
 while (x >= 10) {
-  console.log(x);
-  x = x - 1;
+  console.log(x)
+  x = x - 1
 }
 
-let x = 20;
+let x = 20
 do {
-  console.log(x);
-  x = x - 1;
-} while (x >= 10);
+  console.log(x)
+  x = x - 1
+} while (x >= 10)
 
 // Functions
 function addFive(number) {
-  return number + 5;
+  return number + 5
 }
 
 const addSix = (number) => {
-  return number + 6;
-};
+  return number + 6
+}
 
 const addSeven = function (number) {
-  return number + 7;
-};
+  return number + 7
+}
