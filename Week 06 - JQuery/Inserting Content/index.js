2090ff6e67a2729fed25d7d727717020fd92aa32
Mark Williams <markwilliams@marks-mbp-2.lan>
2022-02-15 17:18:47 -0600

    more stuff

--- Week 06 - JQuery/Inserting Content/index.js
+++ Week 06 - JQuery/Inserting Content/index.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
   // Add Text
-  $('#h1-text').click(() => {
-    $('#main').append('This is new...');
-  });
+  $("#h1-text").click(() => {
+    $("#main").append("This is new...")
+  })
 
   // Add Content: adds to inside of element (nested)
-  $('#h1-content').click(() => {
-    let heading = '<h1>Added Text:</h1>';
-    let paragraph = document.createElement('p');
-    paragraph.innerHTML = '<em>I am ready for six figures!</em>';
-    $('body').append(heading, paragraph);
-  });
+  $("#h1-content").click(() => {
+    let heading = "<h1>Added Text:</h1>"
+    let paragraph = document.createElement("p")
+    paragraph.innerHTML = "<em>I am ready for six figures!</em>"
+    $("body").append(heading, paragraph)
+  })
 
-  // Add Cotent Before
-  $('#h1-content-before').click(() => {
-    let heading = '<h1>Added Text:</h1>';
-    let paragraph = document.createElement('p');
-    paragraph.innerHTML = '<em>I am ready for six figures!</em>';
-    $('#main').before(heading, paragraph);
-  });
+  // Add Content Before
+  $("#h1-content-before").click(() => {
+    let heading = "<h1>Added Text:</h1>"
+    let paragraph = document.createElement("p")
+    paragraph.innerHTML = "<em>I am ready for six figures!</em>"
+    $("#main").before(heading, paragraph)
+  })
 
   // Add Cotent After
-  $('#h1-content-after').click(() => {
-    let heading = '<h1>Added Text:</h1>';
-    let paragraph = document.createElement('p');
-    paragraph.innerHTML = '<em>I am ready for six figures!</em>';
-    $('#main').after(heading, paragraph);
-  });
-});
+  $("#h1-content-after").click(() => {
+    let heading = "<h1>Added Text:</h1>"
+    let paragraph = document.createElement("p")
+    paragraph.innerHTML = "<em>I am ready for six figures!</em>"
+    $("#main").after(heading, paragraph)
+  })
+})
